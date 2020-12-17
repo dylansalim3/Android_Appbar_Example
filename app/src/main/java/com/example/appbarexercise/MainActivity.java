7523299a7a70f10a2bc425abd5c2f8a048306347
dylan <dylansalim3@hotmail.com>
2020-12-17 19:56:59 +0800

    initial commit

---
+++ app/src/main/java/com/example/appbarexercise/MainActivity.java
@@ -0,0 +1,53 @@
+package com.example.appbarexercise;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar mToolbar = (Toolbar) findViewById(R.id.TB);
+        setSupportActionBar(mToolbar);
+        Button button = (Button) findViewById(R.id.btn_next);
+        button.setOnClickListener(view -> {
+            Intent intent = new Intent(this,ChildActivity.class);
+            startActivity(intent);
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_favorite:
+                displayToast("Favorite button pressed");
+                break;
+            case R.id.action_settings:
+                displayToast("Settings button pressed");
+                break;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void displayToast(String msg) {
+        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
+    }
+}
